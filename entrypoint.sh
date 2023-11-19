e32406343d9695fc61e1ffd289be8c8b3e19c836
aslon1213 <hamidovaslon1@gmail.com>
2023-11-20 01:55:38 +0500

    reverse proxy for employer and job_seeker implemented in golang

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+cd ./main_gateway
+go run cmd/main_gateway/main.go 
+
+
+# cd ..
+# cd employer
+# source ./bin/activate
+# uvicorn main:app --reload --port 8000 &
+# cd ..
+# cd job_seeker
+# source ./bin/activate
+# uvicorn main:app --reload --port 8001 &
